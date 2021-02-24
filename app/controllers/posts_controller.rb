8bc7d2186a62e957e8ee91c371991a377692fe9b
Yu Shi <yushiys95@gmail.com>
2021-02-24 01:38:47 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,18 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  def create
+    # Create a new Post instance
+    @post = Post.new
+    # Pass in the parameters from the form
+    @post.title = params[:title]
+    @post.description = params[:description]
+    # Save the record
+    @post.save
+  # Update the code with a redirect that leverages a route helper method
+  # Refactor the post variable into an instance variable
+    redirect_to post_path(@post)
+  end
 end
+
+
